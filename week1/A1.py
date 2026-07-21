a0e3ce89e08c43c4ea151fd0ad0d663c7c4756b3
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-07-21 12:36:13 +0300

    изменения после кодревью

--- week1/A1.py
+++ week1/A1.py
@@ -10,24 +10,27 @@ class Vector:
     def __add__(self, v):
         if isinstance(v, Vector):
             return Vector(self.x1 + v.x1, self.x2 + v.x2)
-        raise TypeError(f'Cannot add a Vector with {type(v)} using this operator')
+        return NotImplemented
 
     def __sub__(self, v):
         if isinstance(v, Vector):
             return Vector(self.x1 - v.x1, self.x2 - v.x2)
-        raise TypeError(f'Cannot subtract a Vector with {type(v)} using this operator')
+        return NotImplemented
 
     def __mul__(self, v):
-        if isinstance(v, int) or isinstance(v, float):
+        if isinstance(v, (int,float)):
             return Vector(v * self.x1, v * self.x2)
-        raise TypeError(f'Cannot multiply a Vector with {type(v)} using this operator')
+        return NotImplemented
 
     __rmul__ = __mul__
 
     def __eq__(self, v):
         if isinstance(v, Vector):
             return self.x1 == v.x1 and self.x2 == v.x2
-        raise TypeError(f'Cannot compare a Vector with {type(v)} using this operator')
+        return NotImplemented
+
+    def __hash__(self):
+        return hash((self.x1, self.x2))
 
     def __abs__(self):
         return (self.x1**2 + self.x2**2) ** 0.5
@@ -35,11 +38,17 @@ class Vector:
     def __str__(self):
         return f'({self.x1}, {self.x2})'
 
+    def __repr__(self):
+        return f'Vector({self.x1}, {self.x2})'
+
     def __matmul__(self, v):
         if isinstance(v, Vector):
             return self.x1 * v.x1 + self.x2 * v.x2
-        raise TypeError(f'Cannot multiply a Vector with {type(v)} using this operator')
+        return NotImplemented
 
-v1 = Vector(1, 2)
-v2 = Vector(3, 4)
-print(v1 + v2, v1 - v2, v1 @ v2, abs(v1), v1 == v2)
+
+if __name__ == '__main__':
+    v1 = Vector(1, 2)
+    v2 = Vector(3, 4)
+    print(v1 + v2, v1 - v2, v1 @ v2, abs(v1), v1 == v2)
+    print({v1: v2})
