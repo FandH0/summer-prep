3dcd04b37113b59109f4e4c902245372376e5447
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-07-21 13:04:50 +0300

    поправки кодревью, применение модуля abc: класс ABC, декоратора abstractmethod

--- week1/A2.py
+++ week1/A2.py
@@ -1,11 +1,11 @@
 # Базовый класс Shape с методом area(), наследники Circle, Rectangle, Triangle.
 # Функция total_area(shapes) принимает список любых фигур и возвращает сумму площадей.
 # Попытка создать Shape напрямую должна вызывать исключение.
-from math import pi, sqrt
+from math import pi, sqrt, isclose
+from abc import ABC, abstractmethod
 
-class Shape:
-    def __init__(self):
-        raise NotImplementedError('Implemented only as a parent class')
+class Shape(ABC):
+    @abstractmethod
     def area(self): pass
 
 class Circle(Shape):
@@ -33,14 +33,15 @@ class Triangle(Shape):
         p = (self.a + self.b + self.c) / 2
         return sqrt(p * (p-self.a) * (p-self.b) * (p-self.c))
 
+
 def total_area(shapes: list[Shape]):
-    return sum(map(lambda x: x.area(), shapes))
+    return sum(x.area() for x in shapes)
 
 if __name__ == "__main__":
     a, b, c = Rectangle(1, 2), Circle(1), Triangle(3, 4, 5)
     try:
         d = Shape()
         assert False
-    except NotImplementedError:
+    except TypeError:
         pass
-    assert total_area([a, b, c]) == 2 + pi + 6
+    assert isclose(total_area([a, b, c]), 8 + pi)
