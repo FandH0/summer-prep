f71b5c21511e1289f9b321497453eb811b8fdccb
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-07-21 14:28:26 +0300

    исправление, чистка текста через translate, maketrans и string.punctuation

--- week1/A4.py
+++ week1/A4.py
@@ -1,31 +1,24 @@
 # Дан текст. Топ-5 частых слов (регистр игнорировать, пунктуацию убрать) в формате
 # «слово:количество». Двумя способами: Counter и вручную через defaultdict(int); сравнить код
 from collections import Counter, defaultdict
+from string import punctuation
+
+
+def normalize(text: str) -> list[str]:
+    if not isinstance(text, str):
+        raise TypeError("Неверный тип текста")
+    return text.lower().translate(str.maketrans('', '', punctuation)).split()
+
 # Counter
-def word_counter(text: str):
-    if not isinstance(text, str): raise ValueError
+def word_counter(text: str) -> dict[str: int]:
+    text = normalize(text) # type(text) = list
+    count = Counter(text)
 
-    punc = (',', ',', '!', '?', ';', '"', "'")
-    for i in punc:
-        text = text.replace(i, '')
-
-    text = text.lower()   # type(text) = str
-    text = text.split()   # type(text) = list
-    text = Counter(text)  # type(text) = Counter i.e word:count
-
-    top_words = sorted(text.keys(), key=text.get, reverse=True)[:5]
-    return {word: text[word] for word in top_words}
+    return dict(count.most_common(5))
 
 # defaultdict
-def word_default(text: str):
-    if not isinstance(text, str): raise ValueError
-
-    punc = (',', ',', '!', '?', ';', '"', "'")
-    for i in punc:
-        text = text.replace(i, '')
-
-    text = text.lower()   # type(text) = str
-    text = text.split()   # type(text) = list
+def word_default(text: str) -> dict[str: int]:
+    text = normalize(text)  # type(text) = list
     count = defaultdict(int)
     for word in text:
         count[word] += 1
@@ -33,6 +26,9 @@ def word_default(text: str):
     top_words = sorted(count.keys(), key=count.get, reverse=True)[:5]
     return {word: count[word] for word in top_words}
 
+
 if __name__ == "__main__":
     example = "This example shows how the __missing__() method works behind the scenes in defaultdict. \n It is automatically called when a key is not found, returning the default value instead of raising a KeyError."
     assert word_counter(example) == word_default(example)
+    assert word_counter('a, a , d a f f') == {'a': 3, 'f': 2, 'd': 1}
+    assert word_default('a, a , d a f f') == {'a': 3, 'f': 2, 'd': 1}
