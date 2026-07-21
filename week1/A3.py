a8c6193ce82a0bb34faa1850ff32f9f92d6fbc7b
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-07-21 13:32:20 +0300

    исправление бага и краевых условий

--- week1/A3.py
+++ week1/A3.py
@@ -3,14 +3,15 @@
 # list(chunked(range(7), 3)) → [[0,1,2],[3,4,5],[6]].
 # Запрещено предварительно превращать вход в список целиком — вход может быть бесконечным генератором.
 
-def chunked(iterator, n: int):
+def chunked(iterable, n: int):
+    if not n > 0: raise ValueError
     chunk = []
-    for i in iterator:
+    for i in iterable:
         chunk.append(i)
         if len(chunk) == n:
             yield chunk
             chunk = []
-    else:
+    if chunk:
         yield chunk
 
 if __name__ == '__main__':
