28150b48b3eda6ba10fd299130fee9f5bd90ddd3
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-07-29 19:33:52 +0300

    fix(B3): исправление ошибки с удалением последнего символа строки

--- week2/B3.py
+++ week2/B3.py
@@ -1,15 +1,15 @@
 import sys
 
 
-s = sys.stdin.readline()[:-1]
-chs = set()
+s = sys.stdin.readline().removesuffix('\n')
+window = set()
 left = 0
-m = 0
+max_len = 0
 for right, ch in enumerate(s):
-    while ch in chs:
-        chs.remove(s[left])
+    while ch in window:
+        window.remove(s[left])
         left += 1
-    chs.add(ch)
-    m = max(m, right - left + 1)
+    window.add(ch)
+    max_len = max(max_len, right - left + 1)
 
-print(m)
+print(max_len)
