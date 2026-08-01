7dfe37ab778fb052fada2f6f6ad6f93c2febe67b
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-08-01 19:31:59 +0300

    fix(C2): предзагрузка элементов в int, чтобы избежать повторного перевода str -> int

--- week3/С2.py
+++ week3/С2.py
@@ -1,20 +1,18 @@
 import sys
 
 
-# перевод в int по мере требования
-data = sys.stdin.read().split()
-n = int(data[0])
-q = int(data[n + 1])
+data = list(map(int, sys.stdin.read().split()))
+n = data[0]
+q = data[n + 1]
 answers = []
 
 
 def bisect_left(goal):
-    global data
     left, right = 1, n + 1
     while left != right:
         mid = (left + right) // 2
-        mid_num = int(data[mid])
-        if mid_num == goal and (int(data[mid - 1]) < mid_num or mid == 1):
+        mid_num = data[mid]
+        if mid_num == goal and (data[mid - 1] < mid_num or mid == 1):
             return mid - 1  # учитываем сдвиг в 1 при подборе left, right
         elif mid_num < goal:
             left = mid + 1
@@ -24,12 +22,11 @@ def bisect_left(goal):
 
 
 def bisect_right(goal):
-    global data
     left, right = 1, n + 1
     while left != right:
         mid = (left + right) // 2
-        mid_num = int(data[mid])
-        if mid_num == goal and (int(data[mid + 1]) > mid_num or mid == n):
+        mid_num = data[mid]
+        if mid_num == goal and (data[mid + 1] > mid_num or mid == n):
             return mid - 1  # учитываем сдвиг в 1 при подборе left, right
         elif mid_num > goal:
             right = mid
@@ -39,7 +36,7 @@ def bisect_right(goal):
 
 
 for i in range(n + 2, n + q + 2):
-    goal = int(data[i])
+    goal = data[i]
     answers.append((bisect_left(goal), bisect_right(goal)))
 
 sys.stdout.write("\n".join(f"{i[0]} {i[1]}" for i in answers))
