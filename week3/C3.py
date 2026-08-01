f52e917667f73f2276bb964638ce51c32903fac8
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-08-01 20:19:17 +0300

    feat(C3): решение через бинарный поиск по длине веревки с поиском правой границы

---
+++ week3/C3.py
@@ -0,0 +1,24 @@
+import sys
+
+
+n, k, *nums = map(int, sys.stdin.read().split())
+
+
+def count_ropes(length):
+    ans = 0
+    for rope in nums:
+        ans += rope // length
+    return ans
+
+
+left, right = 1, 1 + sum(nums) // k
+answer = 0
+while left != right:
+    mid = (left + right) // 2
+    amount = count_ropes(mid)
+    if amount >= k:
+        answer = mid
+        left = mid + 1
+    else:
+        right = mid
+print(answer)
