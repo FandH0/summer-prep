ac026f7edc1b5997e8479fd4b3c21611315a7433
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-08-01 19:32:48 +0300

    fix(C1): предзагрузка элементов в int, чтобы избежать повторного перевода str -> int

--- week3/С1.py
+++ week3/С1.py
@@ -2,17 +2,17 @@ import sys
 
 
 # перевод в int по мере требования
-data = sys.stdin.read().split()
-n = int(data[0])
-q = int(data[n + 1])
+data = list(map(int, sys.stdin.read().split()))
+n = data[0]
+q = data[n + 1]
 answers = []
 
 for i in range(n + 2, n + q + 2):
     left, right = 1, n + 1  # data indices
-    num = int(data[i])
+    num = data[i]
     while left < right:
         mid = (left + right) // 2
-        mid_num = int(data[mid])
+        mid_num = data[mid]
         if mid_num > num:
             right = mid
         elif mid_num < num:
