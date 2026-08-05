de7502d7bbaddd3180863486e15c727bad07b83e
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-08-05 12:04:34 +0300

    refactor(C5): изменение ввода и вывода при запросе на ввод и вывод строго в начале и конце программы

--- week3/C5.py
+++ week3/C5.py
@@ -7,16 +7,20 @@ cur_min содержит информацию о данном минимуме, 
 до добавления этого элемента. Таким образом при появлении нового минимума, он в паре к себе запоминает прошлый минимум,
 к которому мы получим доступ при его удалении, и обновляет cur_min.
 """
-q = int(sys.stdin.readline().rstrip())
+_, *ops = sys.stdin.read().split("\n")
 stack = []  # числа хранятся в виде: (число, минимальный элемент в стеке лежащий до данного числа)
 cur_min = float("inf")
-for i in range(q):
-    op = sys.stdin.readline().split()
-    if "push" in op:
+answer = []
+
+for op in ops:
+    op = op.split()
+    if op[0] == "push":
         num = int(op[1])
         stack.append((num, cur_min))
         cur_min = min(num, cur_min)
-    elif "min" in op:
-        print(cur_min)
+    elif op[0] == "min":
+        answer.append(cur_min)
     else:
         cur_min = stack.pop()[1]
+
+print(*answer, sep='\n')
