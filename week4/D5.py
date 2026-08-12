b3e9809afe9d61d7fe3def809999c1943315f130
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-08-12 16:21:09 +0300

    fix(D5): исправление ошибки подсчета при ширине в 1 на dfs_island_iterative, приведение функции к общему виду с bfs_island

--- week4/D5.py
+++ week4/D5.py
@@ -34,35 +34,30 @@ def dfs_islands_recursive():
 def dfs_island_iterative():
     islands = 0
     visited = [[False] * m for _ in range(n)]
-    stack = [(0, 0, False)]
-    pointer_y, pointer_x = 0, 1
-    while stack:
-        y, x, flood = stack.pop()
-        if not stack and pointer_y < n:  # основной проход
-            while pointer_x < m:
-                stack.append((pointer_y, pointer_x, False))
-                pointer_x += 1
-            pointer_y += 1
-            pointer_x = 0
 
-        if not flood and not visited[y][x] and field[y][x] == '1':
-            islands += 1
-            flood = True
-
-        if flood and field[y][x] == '1':
-            if 0 <= y + 1 < n and not visited[y + 1][x]:
-                visited[y + 1][x] = True
-                stack.append((y + 1, x, True))
-            if 0 <= y - 1 < n and not visited[y - 1][x]:
-                visited[y - 1][x] = True
-                stack.append((y - 1, x, True))
-            if 0 <= x + 1 < m and not visited[y][x + 1]:
-                visited[y][x + 1] = True
-                stack.append((y, x + 1, True))
-            if 0 <= x - 1 < m and not visited[y][x - 1]:
-                visited[y][x - 1] = True
-                stack.append((y, x - 1, True))
+    def mark_island(y, x):
+        stack = [(y, x)]
         visited[y][x] = True
+        while stack:
+            y, x = stack.pop()
+            if 0 <= y + 1 < n and not visited[y + 1][x] and field[y + 1][x] == '1':
+                stack.append((y + 1, x))
+                visited[y + 1][x] = True
+            if 0 <= y - 1 < n and not visited[y - 1][x] and field[y - 1][x] == '1':
+                stack.append((y - 1, x))
+                visited[y - 1][x] = True
+            if 0 <= x + 1 < m and not visited[y][x + 1] and field[y][x + 1] == '1':
+                stack.append((y, x + 1))
+                visited[y][x + 1] = True
+            if 0 <= x - 1 < m and not visited[y][x - 1] and field[y][x - 1] == '1':
+                stack.append((y, x - 1))
+                visited[y][x - 1] = True
+
+    for y in range(n):
+        for x in range(m):
+            if not visited[y][x] and field[y][x] == '1':
+                mark_island(y, x)
+                islands += 1
 
     return islands
 
