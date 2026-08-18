92c07c6f8bb7d1a219fc92fa71abf0e2e93de6d5
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-08-18 10:50:29 +0300

    fix(E1): исправление двойного добавления k элемента в nums

--- week5/E1.py
+++ week5/E1.py
@@ -4,13 +4,14 @@ import heapq
 
 n, k, *nums = map(int, sys.stdin.read().split())
 k_largest = []
-answer = ['-' for _ in range(k - 1)]
+answer = ['-'] * min(k - 1, n)
 for num in nums:
     if len(k_largest) < k:
         heapq.heappush(k_largest, num)
+    elif num > k_largest[0]:
+        heapq.heappushpop(k_largest, num)
+
     if len(k_largest) == k:
-        if num > k_largest[0]:
-            heapq.heappushpop(k_largest, num)
         answer.append(str(k_largest[0]))
 
 sys.stdout.write('\n'.join(answer))
