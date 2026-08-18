7308c9826be37fa644a35fdac8b40bf896f4a86f
Ilya_Kamyshov <ilyakamyshov@gmail.com>
2026-08-18 16:39:41 +0300

    feat(E3): сумма на отрезке за O(1) времени и количество отрезков данной суммы за один проход

---
+++ week5/E3.py
@@ -0,0 +1,26 @@
+import sys
+from itertools import accumulate
+from collections import defaultdict
+
+
+n, q, s = map(int, sys.stdin.readline().split())
+nums = map(int, sys.stdin.readline().split())
+requests = map(int, sys.stdin.read().split())
+
+pref_sums = [0]
+pref_sums.extend(accumulate(nums))
+
+answer = []
+i = 0
+while i < q:
+    a, b = next(requests), next(requests)
+    answer.append(str(pref_sums[b + 1] - pref_sums[a]))
+    i += 1
+
+prev = defaultdict(int)
+counts = 0
+for n in pref_sums:
+    counts += prev[n - s]
+    prev[n] += 1
+
+sys.stdout.write("\n".join(answer) + '\n' + str(counts))
